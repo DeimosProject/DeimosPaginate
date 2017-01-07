3228b092a6dfc325090d1270d1cd87d48f3d199a
Babichev Maxim <maksim.babichev95@gmail.com>
2017-01-07 22:52:54 +0300

    phpunit

---
+++ tests/Test/Paginate/PaginateTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test\Paginate;
+
+class PaginateTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testEntity()
+    {
+        // todo
+        $this->assertEquals(1, 1);
+    }
+
+}
