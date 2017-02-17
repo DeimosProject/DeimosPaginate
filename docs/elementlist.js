731f372a9f3584da1d9c13c411948358aa2e68fa
REZ1DENT3 <maksim.babichev95@gmail.com>
2017-02-17 08:30:00 +0300

    update docs

--- docs/elementlist.js
+++ docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Deimos\\Paginate\\ExtOrm"],["c","Deimos\\Paginate\\Pager"],["c","Deimos\\Paginate\\Paginate"],["c","Exception"],["c","InvalidArgumentException"],["c","LogicException"],["c","Throwable"]];
+ApiGen.elements = [["c","Deimos\\Paginate\\Pager"],["c","Deimos\\Paginate\\Paginate"],["c","Exception"],["c","InvalidArgumentException"],["c","LogicException"],["c","Throwable"]];
